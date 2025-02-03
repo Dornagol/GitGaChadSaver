077cb95bdbf1cf571e9de9a7c1b50b4ec80bc991
Dornagol <feufeuhugo@gmail.com>
2025-02-03 22:58:49 +0100

    random commit

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use std::process::Command;
 
-fn main()
+fn main() -> Result<(), Box<dyn std::error::Error>>
 {
     let is_git_repo = Command::new("git")
         .arg("rev-parse")
         .arg("--is-inside-work-tree")
         .output()?;
+
     if !is_git_repo.status.success()
     {
         eprintln!("T'es pas dans un repo git 👉🙉");
@@ -15,29 +16,44 @@ fn main()
     git_commit()?;
     git_push()?;
     println!("Repo git bien push 🗿");
-    Ok(());
+    Ok(())
 }
 
-fn git_add()
+fn git_add() -> Result<(), Box<dyn std::error::Error>>
 {
-    let git_addd = Command::new("git")
+    let status = Command::new("git")
         .arg("add")
         .arg(".")
-        .spawn()?;
+        .status()?;
+    
+    if !status.success() {
+        return Err("Echec de 'git add'👉🙉".into());
+    }
+    Ok(())
 }
 
-fn git_commit()
+fn git_commit() -> Result<(), Box<dyn std::error::Error>>
 {
-    let git_addd = Command::new("git")
+    let status = Command::new("git")
         .arg("commit")
         .arg("-m")
-        .arg("'random commit'")
-        .spawn()?;
+        .arg("random commit")
+        .status()?;
+
+    if !status.success() {
+        return Err("Echec de 'git commit'👉🙉".into());
+    }
+    Ok(())
 }
 
-fn git_push()
+fn git_push() -> Result<(), Box<dyn std::error::Error>>
 {
-    let git_addd = Command::new("git")
+    let status = Command::new("git")
         .arg("push")
-        .spawn()?;
+        .status()?;
+
+    if !status.success() {
+        return Err("Echec de 'git push'👉🙉".into());
+    }
+    Ok(())
 }
